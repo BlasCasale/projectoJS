c2096daaeed78c5b20ccc4562bc1af62e72861ee
BlasCasale <casale.blas@live.com>
2023-02-03 09:43:28 -0300

    listo

--- js/carrito.js
+++ js/carrito.js
@@ -1,35 +1,66 @@
-const carrito = localStorage.getItem('carrito')
+let carrito = localStorage.getItem('carrito')
 
-const carritoJSON = JSON.parse(carrito)
-
-const compraFinal = []
-
-carritoJSON.forEach(element => {
-    let nuevaCompra = element.precio
-    compraFinal.push(nuevaCompra)
-    let sumatoria
-    compraFinal.reduce((acc, nuevaCompra)=> sumatoria + nuevaCompra, 0)
-});
-
-console.log(compraFinal)
+let carritoJSON = JSON.parse(carrito)
 
 
+const hacerCarrito = () => {
+    const contenedor = document.getElementById('carrito')
+
+    carritoJSON.forEach((compra) => {
+        const div = document.createElement('div')
+        div.classList.add('card__carro')
+        div.innerHTML += `
+                        <img src="../${compra.img}" alt="" class="card__carro-img">
+
+                        <p class="card__carro-p">${compra.nombre}</p>
+                        
+                        <p class="card__carro-p">$${compra.precio}</p>
+
+                        <button class="btn__tacho">
+                            <img src="../img/tachoBasura.webp" alt="Basura" class="btn__tacho-img" id="${compra.id}">
+                        </button>
+
+        `
+        
+        contenedor.prepend(div)
+    });
+}
+
+hacerCarrito()
 
 
-// const sumarCompra = () => {
-//     let compras
-//     let nuevasCompras
-//     let precioFinal
+// obtengo botones para eliminar uno por uno los elementos
 
-//     carritoJSON.forEach(element => {
-//         nuevasCompras = element.precio
-//         return
-//     });
+const btnTacho = document.getElementById('carrito')
 
-//     precioFinal.reduce((compras, nuevasCompras) => compras + nuevasCompras, precioFinal)
+btnTacho.addEventListener('click', (e) => {
+    console.log(e.target.id)
+    sacarDelCarrito(e.target.id)
+})
 
-//     console.log(compras)
-//     console.log(precioFinal)
-// }
+const sacarDelCarrito = (productoID) => {
+    let id = carritoJSON.find(producto => producto.id == productoID)
 
-// sumarCompra()
+    console.log(id)
+}
+
+
+// para vaciar todo el carrito
+let vaciarCarro = document.getElementById('vaciarCarro')
+
+vaciarCarro.addEventListener('click', () => {
+    
+    // vacio el array y lo paso a LS para salvar los datos
+    carritoJSON = []
+    console.log(carritoJSON)
+    carrito = JSON.stringify(carritoJSON)
+    localStorage.setItem('carrito', carrito)
+
+    // hago el carrito de nuevo
+    let padreDiv = document.getElementById('carrito')
+    let div = document.getElementsByClassName('card__carro')
+
+    padreDiv.removeChild(div)
+
+    hacerCarrito()
+})
