6d03ae0e988517ff0ff5654de3300b0e5412ee90
BlasCasale <casale.blas@live.com>
2023-02-20 23:51:15 -0300

    listo

--- js/pushearCarrito.js
+++ js/pushearCarrito.js
@@ -1,5 +1,3 @@
-import { hacerCards } from "./crearCards.js";
-
 const carrito = []
 
 const carritoLS = JSON.parse(localStorage.getItem('carrito'))
@@ -11,44 +9,17 @@ carrito.push(...carritoLS)
 const contenedor = document.getElementById('padreCards')
 
 contenedor.addEventListener('click', (e) => {
-    searchProduct(e.target.id)
+    agregarAlCarrito(e.target.id)
 })
 
-const searchProduct = (productoID) => {
+
+ // con esta funcion busco los productos por id y los voy pusheando al array
+const agregarAlCarrito = (productoID) => {
     const producto = productos.find(producto => producto.id == productoID)
 
-    findStock(producto)
-}
-
-const findStock = (parameter) => {
-    let {stock} = parameter
-
-    if (stock > 0) {
-            Toastify({
-                text: "Producto agregado",
-                duration: 1500,
-                gravity: "bottom",
-                position: "right",
-                style: {
-                    background: "linear-gradient(90deg, rgba(0,53,84,1) 30%, rgba(121,173,220,1) 83%)",
-                    color: "#fff",
-                    borderRadius: "30px",
-                    border: "solid 2px #110e82"
-                },
-                destination: "pages/carrito.html",
-                newWindow: true,
-                close: true,
-                stopOnFocus: true,
-            }).showToast()
-            stock--
-            let h3stock = document.getElementById('stock')
-            h3stock.innerText = ''
-            h3stock.innerText = `Stock ${stock}`
-            
-            carrito.push(parameter)
-        }else {
+    if (producto != undefined) {
         Toastify({
-            text: "No hay stock",
+            text: "Producto agregado",
             duration: 1500,
             gravity: "bottom",
             position: "right",
@@ -58,19 +29,14 @@ const findStock = (parameter) => {
                 borderRadius: "30px",
                 border: "solid 2px #110e82"
             },
+            destination: "pages/carrito.html",
+            newWindow: true,
+            close: true,
             stopOnFocus: true,
         }).showToast()
+    
+        carrito.push(producto)
+        const carritoJSON = JSON.stringify(carrito)
+        localStorage.setItem('carrito', carritoJSON) 
     }
-
-    const carritoJSON = JSON.stringify(carrito)
-    localStorage.setItem('carrito', carritoJSON) 
 }
-
-
-
-// // con esta funcion busco los productos por id y los voy pusheando al array
-// const agregarAlCarrito = (productoID) => {
-//     const producto = productos.find(producto => producto.id == productoID)
-
-
-// }
--- js/stock.js
+++ js/stock.js
@@ -3,32 +3,31 @@ const productos = []
 const productosLS = JSON.parse(localStorage.getItem('productos'))
 
 class Producto {
-    constructor (nombre, categoria, img, precio, id, stock) {
+    constructor (nombre, categoria, img, precio, id) {
         this.nombre = nombre
         this.categoria = categoria
         this.img = img
         this.precio = precio
         this.id = id
-        this.stock = stock
     }
 }
 
-const ryzen5 = new Producto ("Ryzen 5", "procesador", 'img/ryzen5__4500.webp', 16000, 1, 2)
-const ryzen7 = new Producto ("Ryzen 7", "procesador", 'img/rzyen7__5700G.webp', 18500, 2, 2)
-const ryzen9 = new Producto ("Ryzen 9", "procesador", 'img/ryzen9__5900x.webp', 21000, 3, 2)
-const motherB450m = new Producto ("Mother B450M", "mother", 'img/mother__b450m.webp', 14000, 4, 2)
-const motherB550 = new Producto ("Mother B500", "mother", 'img/mother__b550.webp', 16000, 5, 2)
-const motherX570 = new Producto ("Mother X570", "mother", 'img/mother__x570.webp', 19500, 6, 2)
-const rtx2080 = new Producto ('RTX 2080', "gpu", 'img/rtx2080.webp', 28000, 7, 2)
-const rtx3080 = new Producto ('RTX 3080', "gpu", 'img/rtx3080.webp', 33500, 8, 2)
-const rtx4080 = new Producto ('RTX 4080', "gpu", 'img/rtx4080.webp', 38000, 9, 2)
-const ramKingston = new Producto ('RAM Kingston', "ram", 'img/ram__kingston8gb.webp', 3500, 10, 2)
-const ramCorsair = new Producto ('RAM Corsair', "ram", 'img/ram__corsiar8gb.webp', 4500, 11, 2)
-const ramViper = new Producto ('RAM Viper', "ram", 'img/ram__viper16gb.webp', 6000, 12, 2)
-const hdd1TB = new Producto ('Disco HDD 1TB', 'disco' || 'discos', 'img/discoHDD1TB.webp', 4700, 13, 2)
-const hdd2TB = new Producto ('Disco HDD 2TB', 'disco' || 'discos', 'img/discoHDD2TB.webp', 5250, 14, 2)
-const sdd1TB = new Producto ('Disco SDD 1TB', 'disco' || 'discos', 'img/discoSDD1TB.webp', 7550, 15, 2)
-const sdd2TB = new Producto ('Disco SDD 2TB', 'disco' || 'discos', 'img/discoSDD1TB.webp', 9750, 16, 2)
+const ryzen5 = new Producto ("Ryzen 5", "procesador", 'img/ryzen5__4500.webp', 16000, 1)
+const ryzen7 = new Producto ("Ryzen 7", "procesador", 'img/rzyen7__5700G.webp', 18500, 2)
+const ryzen9 = new Producto ("Ryzen 9", "procesador", 'img/ryzen9__5900x.webp', 21000, 3)
+const motherB450m = new Producto ("Mother B450M", "mother", 'img/mother__b450m.webp', 14000, 4)
+const motherB550 = new Producto ("Mother B500", "mother", 'img/mother__b550.webp', 16000, 5)
+const motherX570 = new Producto ("Mother X570", "mother", 'img/mother__x570.webp', 19500, 6)
+const rtx2080 = new Producto ('RTX 2080', "gpu", 'img/rtx2080.webp', 28000, 7)
+const rtx3080 = new Producto ('RTX 3080', "gpu", 'img/rtx3080.webp', 33500, 8)
+const rtx4080 = new Producto ('RTX 4080', "gpu", 'img/rtx4080.webp', 38000, 9)
+const ramKingston = new Producto ('RAM Kingston', "ram", 'img/ram__kingston8gb.webp', 3500, 10)
+const ramCorsair = new Producto ('RAM Corsair', "ram", 'img/ram__corsiar8gb.webp', 4500, 11)
+const ramViper = new Producto ('RAM Viper', "ram", 'img/ram__viper16gb.webp', 6000, 12)
+const hdd1TB = new Producto ('Disco HDD 1TB', 'disco' || 'discos', 'img/discoHDD1TB.webp', 4700, 13)
+const hdd2TB = new Producto ('Disco HDD 2TB', 'disco' || 'discos', 'img/discoHDD2TB.webp', 5250, 14)
+const sdd1TB = new Producto ('Disco SDD 1TB', 'disco' || 'discos', 'img/discoSDD1TB.webp', 7550, 15)
+const sdd2TB = new Producto ('Disco SDD 2TB', 'disco' || 'discos', 'img/discoSDD1TB.webp', 9750, 16)
 
 
 productos.push(ryzen5, ryzen7, ryzen9, motherB450m, motherB550, motherX570, rtx2080, rtx3080, rtx4080, ramKingston, ramCorsair, ramViper, hdd1TB, hdd2TB, sdd1TB, sdd2TB)
